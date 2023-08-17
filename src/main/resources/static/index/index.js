a74731ef08298663ddfa20539eefa79cf24d1e40
lsy <lsy1026@mediazen.co.kr>
2023-08-17 10:14:51 +0900

    index, index_BP 분리

---
+++ src/main/resources/static/index/index.js
@@ -0,0 +1,30 @@
+$.ajax({
+    type: "GET",
+    url: "/valueTest",
+    success: (data) => {
+    	console.log(data);
+    	$('#contents').html(data);
+    }
+});
+
+const mainElement = document.getElementsByClassName('main');
+const MAX_HEIGHT = 3000;
+var newHeight = 0;
+let currentHeight = mainElement.clientHeight;
+
+const waves = document.querySelector('.waves');
+
+//	Add scroll event listener
+window.addEventListener('scroll', function() {
+	console.log("scroll Y : " + window.scrollY);
+
+	//	infinity scroll
+    //mainElement.style.height = `${window.scrollY + 1010}px`;
+    
+    //	MAX height used scroll
+    newHeight = Math.min(currentHeight + window.scrollY, MAX_HEIGHT);
+    mainElement.style.height = `${newHeight}px`;
+	
+    //	wave down scroll
+    //waves.style.transform = `translate3d(0, ${window.scrollY * 0.5}px, 0)`;
+});
