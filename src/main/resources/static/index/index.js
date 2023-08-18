81cfdb93c3f2a574c622d32b671e8af60f2ce8cc
lsy <lsy1026@mediazen.co.kr>
2023-08-18 15:07:37 +0900

    -index templates @media quary ADD

--- src/main/resources/static/index/index.js
+++ src/main/resources/static/index/index.js
@@ -22,9 +22,21 @@ window.addEventListener('scroll', function() {
     //mainElement.style.height = `${window.scrollY + 1010}px`;
     
     //	MAX height used scroll
-    newHeight = Math.min(currentHeight + window.scrollY, MAX_HEIGHT);
-    mainElement.style.height = `${newHeight}px`;
+    //newHeight = Math.min(currentHeight + window.scrollY, MAX_HEIGHT);
+    //mainElement.style.height = `${newHeight}px`;
 	
     //	wave down scroll
     //waves.style.transform = `translate3d(0, ${window.scrollY * 0.5}px, 0)`;
+});
+
+/* @media */
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.querySelector('.menu-button');
+    const sideNav = document.querySelector('.sidenav');
+    console.log("menuButton ready");
+
+    menuButton.addEventListener('click', function() {
+        sideNav.classList.toggle('show');
+        console.log("menuButton action");
+    });
 });
